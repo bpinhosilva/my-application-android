9238405bbf666b37ba32178186391fbaeb66ec5b
Bruno Pinho <bpinhosilva@gmail.com>
2017-01-05 16:10:05 -0200

    First commit.

---
+++ app/src/main/java/com/example/bruno/myapplication/Main.java
@@ -0,0 +1,18 @@
+package com.example.bruno.myapplication;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by bruno on 1/5/17.
+ */
+
+public class Main extends FragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.myapp_main_activity);
+    }
+}
